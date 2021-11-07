ca4fa7b593a92412d2d9191d22a6a27c5f7bd41a
Nikita Borisov <nborisov256@gmail.com>
2021-11-08 00:11:00 +0300

    + slider

--- js/index.js
+++ js/index.js
@@ -6,14 +6,30 @@ window.addEventListener('DOMContentLoaded', function() {
             next = document.querySelector('.next'),
             dots = document.getElementsByClassName("examples__slider-dot"),
             names = document.getElementsByClassName("examples-nav__item"),
+            city = document.querySelector(".examples__details-city"),
+            area = document.querySelector(".examples__details-area"),
+            time = document.querySelector(".examples__details-time"),
+            cost = document.querySelector(".examples__details-cost"),
             info = [
                 {
+                    city: "Rostov-on-Don <br> LCD admiral",
+                    area: "81 m2",
+                    time: "3.5 months",
+                    cost: "Upon request",
                     path: "./img/example1.jpg"
                 },
                 {
+                    city: "Sochi <br> Thieves",
+                    area: "105 m2",
+                    time: "4 months",
+                    cost: "Upon request",
                     path: "./img/example2.jpg"
                 },
                 {
+                    city: "Rostov-on-Don <br> Patriotic",
+                    area: "93 m2",
+                    time: "3 months",
+                    cost: "Upon request",
                     path: "./img/example3.jpg"
                 }
             ];	
@@ -34,6 +50,11 @@ window.addEventListener('DOMContentLoaded', function() {
             }
             dots[index - 1].classList.add("active-dot");
             names[index - 1].classList.add("active-project");
+
+            city.innerHTML = info[index-1].city;
+            area.innerHTML = info[index-1].area;
+            time.innerHTML = info[index-1].time;
+            cost.innerHTML = info[index-1].cost;
         };
     
         prev.addEventListener('click', function() {
